aa061c2f2298cacf014faa1b48146dca8f32cf7c
Andrei <macwin33@gmail.com>
2022-10-10 11:29:22 +0600

    add label

--- totalBill/MainViewController.swift
+++ totalBill/MainViewController.swift
@@ -19,6 +19,20 @@ class MainViewController: UIViewController {
         
     }()
     
+    let descriptionLabel: UILabel = {
+         let label = UILabel()
+        label.text = "Enter the invoice amount and click \"Calculate\""
+        label.textColor = .black
+        label.font = UIFont(name:  "Avenir Next black", size: 15)
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.7
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+        
+    }()
+    
     let logoImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "logo")
@@ -39,6 +53,7 @@ class MainViewController: UIViewController {
         
         view.addSubview(logoImageView)
         view.addSubview(titleLabel)
+        view.addSubview(descriptionLabel)
         
     }
 
@@ -53,7 +68,13 @@ extension MainViewController {
             
             logoImageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
             logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            logoImageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5)
+            logoImageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+            logoImageView.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 15),
+            descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            descriptionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+
         ])
         
     }
